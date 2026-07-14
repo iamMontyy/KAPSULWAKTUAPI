298ab43f0be18fbe1afda94185c71bfc7294b231
Montyyy <danyramadhan558@gmail.com>
2026-07-14 21:44:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
