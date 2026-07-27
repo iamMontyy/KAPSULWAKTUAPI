b5f152e5d0a4e27dd8f38307c129549a0a94cb45
Montyyy <danyramadhan558@gmail.com>
2026-07-27 19:34:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
